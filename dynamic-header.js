57f1ba92ff50f744696b659206fc929ef7f4c336
ulfschneider <u@ulf.codes>
2018-02-18 12:53:01 +0100

    Allow to pick dominant color from image as header background

--- dynamic-header.js
+++ dynamic-header.js
@@ -1,6 +1,8 @@
 /* minifyOnSave, filenamePattern: ./$1.min.$2 */
 
 
+
+
 /*
  * DynamicHeader
  *
@@ -15,6 +17,9 @@ var DynamicHeader = (function() {
     var scrolled, lastScrollTop;
     var header, content, trim;
     var initialHeaderStyle;
+    var colorThiefLib;
+    var colorThief;
+    var color;
     var TRANSITION = 'top 0.2s ease-in-out';
 
     function windowHeight() {
@@ -69,9 +74,9 @@ var DynamicHeader = (function() {
     }
 
     function selectHeader() {
-        header = document.getElementById(config.headerId);
+        header = document.getElementById(self.config.headerId);
         if (!header) {
-            console.error('Header with id=[' + config.headerId + '] could not be found in DOM');
+            console.error('Header with id=[' + self.config.headerId + '] could not be found in DOM');
         } else {
             modifyHeaderStyle();
         }
@@ -129,7 +134,7 @@ var DynamicHeader = (function() {
     function moveHeader() {
         var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
 
-        if (Math.abs(lastScrollTop - scrollTop) <= config.delta) return;
+        if (Math.abs(lastScrollTop - scrollTop) <= self.config.delta) return;
 
         var headerHeight = getHeaderHeight();
         if (scrollTop > lastScrollTop && scrollTop > headerHeight) {
@@ -177,14 +182,81 @@ var DynamicHeader = (function() {
             if (config.overlay) {
                 self.config.overlay = config.overlay;
             }
+            if (config.pickColorFromImgId) {
+                self.config.pickColorFromImgId = config.pickColorFromImgId;
+                loadColorThiefLib();
+            }
+            if (config.lightColor) {
+                self.config.lightColor = config.lightColor;
+            }
+            if (config.darkColor) {
+                self.config.darkColor = config.darkColor;
+            }
+            if (config.opacity) {
+                self.config.opacity = config.opacity;
+            }
             if (config.delta) {
                 self.config.delta = config.delta;
             }
         }
     }
 
+    function getBrightness() {
+        //http://www.w3.org/TR/AERT#color-contrast
+        return (color[0] * 299 + color[1] * 587 + color[2] * 114) / 1000;
+    }
+
+    function isDark() {
+        return getBrightness() < 128;
+    }
+
+    function isLight() {
+        return isDark();
+    }
+
+    function stealColor() {
+        if (self.config.pickColorFromImgId) {
+            var img = document.getElementById(self.config.pickColorFromImgId);
+            if (!img) {
+                console.error('Image with id=[' + self.config.pickColorFromImgId + '] could not be found in DOM');
+            } else if (img.src) {
+                var image = new Image();
+                image.src = img.src;
+                image.onload = function() {
+                    color = colorThief.getColor(image);
+                    header.style.background = 'rgb(' + color.join(',') + ')';
+                    if (isDark() && self.config.lightColor) {
+                        header.style.color = self.config.lightColor;
+                        console.log(header.style.color);
+                    } else if (isLight() && self.config.darkColor) {
+                        header.style.color = self.config.darkColor;
+                        console.log(header.style.color);
+                    }
+                    if (self.config.opacity) {
+                        header.style.background = 'rgb(' + color.join(',') + ',' + self.config.opacity + ')';
+                    }
+                }
+            }
+        }
+    }
+
+    function makeColorThiefInstance() {
+        colorThief = new ColorThief();
+    }
+
+    function loadColorThiefLib() {
+        if (!colorThief) {
+            colorThiefLib = document.createElement('script');
+            colorThiefLib.type = 'text/javascript';
+            colorThiefLib.src = 'color-thief.min.js';
+            colorThiefLib.onload = makeColorThiefInstance;
+            document.getElementsByTagName('head')[0].appendChild(colorThiefLib);
+        }
+    }
+
     function onload() {
         selectHeader();
+        stealColor();
 
         if (header) {
             selectContent();
@@ -202,12 +274,28 @@ var DynamicHeader = (function() {
 
     //public API
     return {
-
+        /*  Without any arguments, DynamicHeader.init() will search for a container
+         *  with id="header" and will make that container the dynamic header.
+         *
+         *  Alternative usage:
+         *  @function DynamicHeader.init(config);
+         *  @param {String} config.headerId - Specify the id of the container you want to make the dynamic header. Default is 'header'.
+         *  @param {Number} config.delta -  The number of pixels a user need to scroll at least in order to make DynamicHeader react on the scrolling. Default is 5.
+         * @param {String} config.pickColorFromImgId - Specify the id of an image from which the dominant color will be extracted and being used as the background color for the header. Default is none. In order to get this working, color-thief.min.js must be placed in the same directory like dyntamic-header.js
+         * @param {String} config.darkColor - The color to use for text inside of a light header. Typically to be used in combination with pickColorFrom ImgId.
+         * @param {String} config.lightColor - The color to use for text inside of a dark header. Typically to be used in combination with pickColorFrom ImgId.
+         * @param {Number} config.opacity - Specify the opacity of the header background with a number from 0 to 1. Default is 1.
+         * @param {Boolean} config.overlay - Specify if the header should be layed over the content without adding additional vertical space for the header at the top of the page. Default is false.
+         *
+         */
         init: function(config) {
             cleanUp();
             transferConfig(config);
             window.addEventListener('load', onload);
         },
+        /*  @function DynamicHeader.destroy();
+         *  Revert all changes that have been made by DynamicHeader.init();
+         */
         destroy: function() {
             cleanUp();
         }
